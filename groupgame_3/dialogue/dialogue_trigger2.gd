e328b9e10ed49b59e44e767f53a7d35b4508e809
NehemiasLucas <nloarcalucas@stu.jsu.edu>
2025-04-25 18:01:24 -0500

    boss done, matei

--- groupgame_3/dialogue/dialogue_trigger2.gd
+++ groupgame_3/dialogue/dialogue_trigger2.gd
@@ -11,10 +11,9 @@ func _process(delta: float) -> void:
 	pass
 
 var dialogue_scene := preload("res://dialogue/dialogue_box.tscn")
-@export var dialogue_text: String = "Hello from the area!"
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		var dialogue = get_parent().get_node("DialogueBox")
-		dialogue.show_dialogue("AH.. you're finally here.. and with the artifact...hmmm..", body.global_position)
+		dialogue.show_dialogue("Familiar Entity: AH.. you're finally here.. and with the artifact...hmmm..", body.global_position)
 	
